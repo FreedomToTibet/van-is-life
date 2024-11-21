83c0895adb58ccb46b84bc0e21e1957ce9260515
FreedomToTibet <98056258+FreedomToTibet@users.noreply.github.com>
2024-11-21 01:16:05 -0800

    adds pathname Login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,23 +1,30 @@
-import { useRef, useEffect } from 'react';
-import { useLoaderData, Form, redirect, useNavigation, useActionData } from 'react-router-dom';
+import {useRef, useEffect} from 'react';
+import {
+  useLoaderData,
+  Form,
+  redirect,
+  useNavigation,
+  useActionData,
+} from 'react-router-dom';
 
 import {loginUser} from '../service/api';
 
-export function loader({ request }) {
+export function loader({request}) {
   return new URL(request.url).searchParams.get('message');
 }
 
-export async function action({ request }) {
+export async function action({request}) {
   const formData = await request.formData();
   const email = formData.get('email');
   const password = formData.get('password');
-  
+	const pathname = new URL(request.url).searchParams.get('redirectTo') || '/host';
+
   try {
-    await loginUser({ email, password });
+    await loginUser({email, password});
     localStorage.setItem('loggedin', true);
-    return redirect('/host');
+    return redirect(pathname);
   } catch (err) {
-		console.error(err);
+    console.error(err);
     return err.message;
   }
 }
@@ -25,38 +32,28 @@ export async function action({ request }) {
 const Login = () => {
   const message = useLoaderData();
   const navigation = useNavigation();
-	const errorMessage = useActionData();
+  const errorMessage = useActionData();
   const formRef = useRef();
 
-	useEffect(() => {
+  useEffect(() => {
     if (navigation.state === 'idle') {
       // Reset the form inputs after submission
       formRef.current.reset();
     }
   }, [navigation.state]);
 
-  // function handleSubmit(e) {
-  //   e.preventDefault();
-  //   setStatus('submitting');
-  //   loginUser(loginFormData)
-  //     .then((data) => {
-  //       navigate("/host", {replace: true});
-  //       setLoginFormData({email: '', password: ''});
-  //     })
-  //     .catch((err) => console.error(err))
-  //     .finally(() => setStatus('idle'));
-  // }
-
   return (
     <div className="login-container">
-			{message && <h2 style={{color: 'red'}}>{message}</h2>}
+      {message && <h2 style={{color: 'red'}}>{message}</h2>}
       {errorMessage && <h2 style={{color: 'red'}}>{errorMessage}</h2>}
 
       <h1>Sign in to your account</h1>
       <Form method="post" className="login-form" ref={formRef} replace>
         <input name="email" type="email" placeholder="Email address" />
         <input name="password" type="password" placeholder="Password" />
-        <button>Log in</button>
+        <button disabled={navigation.state === 'submiting'}>
+          {navigation.state === 'submitting' ? 'Logging in...' : 'Log in'}
+        </button>
       </Form>
     </div>
   );
--- util.js
+++ util.js
@@ -1,9 +1,10 @@
 import { redirect } from "react-router-dom";
 
-export const requireAuth = async () => {
+export const requireAuth = async (request) => {
+	const pathname = new URL(request.url).pathname;
 	const isLoggedIn = localStorage.getItem('loggedin') === 'true';
     
     if (!isLoggedIn) {
-        throw redirect("/login?message=You must log in first.");
+        throw redirect(`/login?message=You must log in first.&redirectTo=${pathname}`);
     }
 };
